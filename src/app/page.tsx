d542b8b59801c166c7f30d9d3bd631d247e8a0c3
Louis Mollick <louismollick@gmail.com>
2024-01-11 00:01:01 -0500

    feat: use pwd.cmd() for ichiran path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { type Metadata } from "next";
 import { Button } from "~/components/ui/button";
 import createWordSegmentation from "~/defer/createWordSegmentation";
 import childProcess from "child_process";
+import path from "path";
 
 export const metadata: Metadata = {
   title: "Lyricsu",
@@ -15,11 +16,16 @@ export const metadata: Metadata = {
 };
 
 const test = () => {
-  const output = childProcess.spawnSync("./ichiran-cli", ["--help"], {
-    encoding: "utf8",
-  });
-  console.log("NORMAL OUTPUT: " + output.stdout);
-  console.log("NORMAL ERROR: " + JSON.stringify(output.error));
+  try {
+    const cmd = path.join(process.cwd(), "src/defer/ichiran-cli");
+    const output = childProcess.spawnSync(cmd, ["--help"], {
+      encoding: "utf8",
+    });
+    console.log("NORMAL OUTPUT: " + output.stdout);
+    console.log("NORMAL ERROR: " + JSON.stringify(output.error));
+  } catch (error) {
+    console.log("NORMAL THROWS: " + JSON.stringify(error));
+  }
 };
 
 export default async function Lyrics() {
--- src/defer/createWordSegmentation.ts
+++ src/defer/createWordSegmentation.ts
@@ -1,13 +1,19 @@
 import { defer } from "@defer/client";
 import childProcess from "child_process";
+import path from "path";
 
 async function createWordSegmentation() {
   return new Promise((resolve) => {
-    const output = childProcess.spawnSync("./ichiran-cli", ["--help"], {
-      encoding: "utf8",
-    });
-    console.log("DEFER OUTPUT: " + output.stdout);
-    console.log("DEFER ERROR: " + JSON.stringify(output.error));
+    try {
+      const cmd = path.join(process.cwd(), "src/defer/ichiran-cli");
+      const output = childProcess.spawnSync(cmd, ["--help"], {
+        encoding: "utf8",
+      });
+      console.log("DEFER OUTPUT: " + output.stdout);
+      console.log("DEFER ERROR: " + JSON.stringify(output.error));
+    } catch (error) {
+      console.log("DEFER THROWS: " + JSON.stringify(error));
+    }
     resolve("done");
   });
 }
